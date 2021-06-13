52fc403b643b0760abd26300247fa704d94d3cbb
Stefan Berg <isbeorn86@googlemail.com>
2021-06-14 00:17:39 +0200

    add schema validation

--- gather.js
+++ gather.js
@@ -4,8 +4,201 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const addFormats = require('ajv-formats');
+addFormats(ajv);
+
 const root = path.join(__dirname, 'manifests');
 
+const schema = {
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'type': 'object',
+    'properties': {
+        'Name' : {
+            'type': 'string'
+        },
+        'Identifier' : {
+            'type': 'string'
+        },
+        'Version' : {
+            'type': 'object',
+            'properties': {
+                'Major': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Minor': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Patch': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Build': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                }
+            },
+              'required': ['Major', 'Minor', 'Patch', 'Build']
+        },
+        'Author' : {
+            'type': 'string'
+        },
+        'Homepage' : {
+            'type': 'string',
+            'format': 'uri-reference'
+        },
+        'Repository' : {
+            'type': 'string',
+            'format': 'uri-reference'
+        },
+        'License' : {
+            'type': 'string'
+        },
+        'LicenseURL' : {
+            'type': 'string',
+            'format': 'uri-reference'
+        },
+        'Tags': {
+            'type': 'array',
+            'items': {
+                'type': 'string'
+            }
+        },
+        'MinimumApplicationVersion': {
+            'type': 'object',
+            'properties': {
+                'Major': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Minor': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Patch': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                },
+                'Build': {
+                    'anyOf': [
+                        {
+                            'type': 'string',
+                            'pattern': '^\\d+$'
+                        },
+                        {
+                            'type': 'number'
+                        }
+                    ]
+                }
+            },
+              'required': ['Major', 'Minor', 'Patch', 'Build']
+        },
+        'Descriptions': {
+            'type': 'object',
+            'properties': {
+                'ShortDescription': {
+                    'type': 'string'
+                },
+                'LongDescription': {
+                    'type': 'string'
+                },
+                'FeaturedImageURL': {
+                    'type': 'string',
+                    'format': 'uri-reference'
+                },
+                'ScreenshotURL': {
+                    'type': 'string',
+                    'format': 'uri-reference'
+                },
+                'AltScreenshotURL': {
+                    'type': 'string',
+                    'format': 'uri-reference'
+                }
+            },
+              'required': ['ShortDescription']
+        },
+
+        'Installer': {
+            'type': 'object',
+            'properties': {
+                'URL': {
+                    'type': 'string',
+                    'format': 'uri-reference'
+                },
+                'Checksum': {
+                    'type': 'string',
+                    'minLength': 32,
+                    'maxLength': 64,
+                    'pattern': '^[A-Fa-f0-9]{32,64}$'
+                },
+                'ChecksumType': {
+                    'type': 'string',
+                    'enum': ['MD5','SHA1','SHA256']
+                },
+                'Type': {
+                    'type': 'string',
+                    'enum': ['DLL', 'ARCHIVE']
+                }
+            },
+              'required': ['URL', 'Checksum', 'ChecksumType', 'Type']
+        }
+    },
+    'required': ['Name', 'Identifier', 'Version', 'Author', 'Repository', 'License', 'LicenseURL', 'MinimumApplicationVersion', 'Descriptions', 'Installer'],
+    'additionalProperties': false
+};
+const validate = ajv.compile(schema);
 
 async function scanDir(dir) {
     let jsonArr = [];
@@ -20,7 +213,14 @@ async function scanDir(dir) {
             console.log('Found manifest in ' + fullPath);
             const data = await readFile(fullPath);
             const json = JSON.parse(data.toString('utf8').replace(/^\uFEFF/, ''));
-            jsonArr.push(json);
+            const valid = validate(json);
+            if(!valid) {
+                console.log('INVALID MANIFEST! ' + fullPath);
+                console.log(JSON.stringify(validate.errors));
+            } else {
+                console.log('Manifest valid at ' + fullPath);
+                jsonArr.push(json);
+            }            
         }        
     }
     return jsonArr;
