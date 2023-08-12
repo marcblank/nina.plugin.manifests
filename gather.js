4948913e37e6d94c1ca99f9229ba78e4a45d866b
Stefan Berg <isbeorn86@googlemail.com>
2023-08-13 00:18:17 +0200

    Enhance gather script to additionally split manifest combination into netcore and netframework as well as creating a meta netcore manifest without descriptions

--- gather.js
+++ gather.js
@@ -16,6 +16,9 @@ const validate = ajv.compile(schema);
 
 async function scanDir(dir) {
     let manifests = [];
+    let manifestsNetFramework = [];
+    let manifestsNetCore = [];
+    let manifestsNetCoreMeta = [];
     const report = {
         failed: 0,
         successful: 0,
@@ -27,12 +30,15 @@ async function scanDir(dir) {
         const fullPath = path.join( dir, file );
         
         if(fs.lstatSync(fullPath).isDirectory()) {
-            const {manifests: jsons, report: report2} = await scanDir(fullPath);
+            const {manifests: jsons, manifestsNetFramework: netFrameworkJsons, manifestsNetCore: netCoreJsons, manifestsNetCoreMeta:netCoreMetaJsons, report: report2} = await scanDir(fullPath);
             report.total += report2.total;
             report.failed += report2.failed;
             report.invalid += report2.invalid;
             report.successful += report2.successful;
             manifests = [...manifests, ...jsons];
+            manifestsNetFramework = [...manifestsNetFramework, ...netFrameworkJsons];
+            manifestsNetCore = [...manifestsNetCore, ...netCoreJsons];
+            manifestsNetCoreMeta = [...manifestsNetCoreMeta, ...netCoreMetaJsons];
         } else {
             try {
                 console.log('\x1b[0m', 'Found manifest in ' + fullPath);
@@ -48,6 +54,13 @@ async function scanDir(dir) {
                     report.successful++;
                     console.log("\x1b[32m",'Manifest valid at ' + fullPath);
                     manifests.push(json);
+                    if(parseInt(json.MinimumApplicationVersion.Major) < 3) {
+                        manifestsNetFramework.push(json);
+                    } else {
+                        manifestsNetCore.push(json);
+                        const { Descriptions, ChangelogURL, License, LicenseURL, Repository, Homepage, ...meta } = json;
+                        manifestsNetCoreMeta.push(meta);
+                    }
                 }    
             } catch(e) {
                 console.log('\x1b[31m', 'INVALID MANIFEST! ' + e.message);
@@ -55,7 +68,7 @@ async function scanDir(dir) {
             }        
         }        
     }
-    return {manifests, report};
+    return {manifests, manifestsNetCore, manifestsNetCoreMeta, manifestsNetFramework, report};
 }
 
 function validateIds(manifests) {
@@ -75,13 +88,22 @@ function validateIds(manifests) {
 
 (async ()=>{
     console.log('Scanning for manifests in ' + root);
-    const {manifests, report} = await scanDir(root);
+    const {manifests, manifestsNetCore, manifestsNetCoreMeta, manifestsNetFramework, report} = await scanDir(root);
     const idcollision = validateIds(manifests);
     const targetDir = __dirname + '/Plugins';
     if (!fs.existsSync(targetDir)){
         fs.mkdirSync(targetDir);
     }
     await writeFile(targetDir + '/manifests.json', JSON.stringify(manifests), function(err, result) {
+        if(err) console.log('\x1b[31m','error', err);
+    });
+    await writeFile(targetDir + '/manifestsNetCore.json', JSON.stringify(manifestsNetCore), function(err, result) {
+        if(err) console.log('\x1b[31m','error', err);
+    });
+    await writeFile(targetDir + '/manifestsNetCoreMeta.json', JSON.stringify(manifestsNetCoreMeta), function(err, result) {
+        if(err) console.log('\x1b[31m','error', err);
+    });
+    await writeFile(targetDir + '/manifestsNetFramework.json', JSON.stringify(manifestsNetFramework), function(err, result) {
         if(err) console.log('\x1b[31m','error', err);
     });
     console.log('\x1b[0m',`done - total: ${report.total} total, ${report.successful} successful, ${report.failed} failed, ${report.invalid} invalid`);
